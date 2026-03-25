a0f5256621b2b9193626d3be0a4db7ce346c1152
ShobhitKumar6 <riyanshpatel894@gmail.com>
2026-03-25 13:30:04 +0530

    session

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
 
-import express, { text } from 'express';
-import nodemailer from 'nodemailer';
+import express from 'express';
+import session from 'express-session';
 
 const app = express();
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'riyanshpatel894@gmail.com',
-        pass: 'your app password'
-    }
-});
-
-// app.use(express.urlencoded({extended:false}))
-app.use(express.json())
 app.set('view engine', 'ejs')
 
-app.get("/mail", (req, resp) => {
-    resp.render("mail")
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+app.use(express.urlencoded({extended:true}))
+
+app.get("/login", (req, resp) => {
+    resp.render("login")
 })
 
-app.post("/submit-email", (req, resp) => {
-    console.log(req.body)
+app.post("/profile", (req, resp) => {
+    req.session.data = req.body;
+    // console.log(req.session.data)
+    resp.render('profile')
+});
 
-    const mailOption = {
-        from: 'riyanshpatel894@gmail.com',
-        to:'riyanshpatel894@gmail.com',
-        subject:req.body.subject,
-        text:req.body.mail
-    }
-    transporter.sendMail(mailOption,(error,info)=>{
-        if(error){
-            resp.send("email operation filed, try again")
-        }else{
-            resp.send("mail send")
-        }
-    })
-    resp.send("email send")
+app.get("/", (req, resp) => {
+    const data = req.session.data;
+    // console.log("data",data);
+
+    resp.render("home",{data})
 })
 
 app.listen(3200);
